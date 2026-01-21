21bc1102f87d0dd86d914ba2743eb3a32d94e69c
kuromamekuromame88 <tei8sia@outlook.jp>
2026-01-21 19:49:19 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,7 @@ void setup() {
 
 void loop() {
   Gamepad.rightStick(map(analogRead(15), 0, 4095, -128, 127), map(analogRead(16), 0, 4095, -128, 127));
+  delay(5);
   /*
   if (command.startsWith("PRESS_BUTTON_")) {
     String buttonStr = command.substring(13); // Extract number after "PRESS_BUTTON_"
